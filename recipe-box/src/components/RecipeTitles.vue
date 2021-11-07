21b625c87ac9df8f014b6d3db1503a8b0977ea15
Sebastian041992 <78859288+Sebastian041992@users.noreply.github.com>
2021-11-07 11:37:44 +0200

    added json db

--- recipe-box/src/components/RecipeTitles.vue
+++ recipe-box/src/components/RecipeTitles.vue
@@ -3,7 +3,7 @@
     <table class="table table-hover table-dark w-100 text-left m-0">
       <tbody>
         <tr>
-          <td class="p-3">1</td>
+          <td>{{ recipe.title }}</td>
         </tr>
         <tr>
           <td class="p-3">2</td>
@@ -18,6 +18,18 @@
 <script>
 export default {
   name: "RecipeTitles",
+  props: ["title"],
+  data() {
+    return {
+      recipe: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/recipe" + this.title)
+      .then((res) => res.json())
+      .then((data) => (this.recipe = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 <style scoped>
